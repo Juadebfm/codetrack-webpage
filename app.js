2439f058d598c686e18fb1e050bfeebbbc2036d5
Julius Gabriel Adebowale <juadebgabriel@gmail.com>
2026-07-25 13:34:05 +0100

    feat: practicing JS

---
+++ app.js
@@ -0,0 +1,84 @@
+// const name = "juadeb909[[]jdndkwndnswd";
+
+// const price = 1000; // integer = whole numbers e.g 10, 1000, 2000 and float = decimal numbers e.g 10.4, 3.2,
+
+// const isLoggedIn = false; // boolean
+
+// const juadebHobbies = [
+//   "COD",
+//   "FIFA",
+//   "Football",
+//   "Club",
+//   10000,
+//   true,
+//   [10, 10],
+// ]; // arrays
+
+// const juadebBiodata = {
+//   name: "Juadeb Gabriel Femi",
+//   age: 37,
+//   location: "Lagos",
+//   skills: [
+//     {
+//       list: ["ReactJS/TS", "Angular", "Python"],
+//     },
+//     "Product",
+//     "Talent",
+//     "DevOps",
+//     "Design",
+//   ],
+// };
+
+// Aritmetic Operators for operations
+// const a = 5;
+// const b = 2;
+
+// const add = a + b;
+// const multiply = a * b;
+// const divide = a / b;
+// const subtract = a - b;
+// const raisedToPower = a ** b;
+// const remainder = a % b; // it divides the lat value from the first but it does not return the computation value, it returns the remainder
+
+// console.log(add);
+// console.log(multiply);
+// console.log(divide);
+// console.log(subtract);
+// console.log(raisedToPower);
+// console.log(remainder);
+
+// Comparison - what does it resolve to, the ans is that it resolves to boolean of either "true" or "false"
+const a = 15;
+const b = 5;
+
+const greater = a > b; // true
+const lesser = a < b; // false
+const greaterOrEqual = a >= b; // true
+const lesserOrEqual = a <= b; // false
+
+console.log("greater", greater);
+console.log(lesser);
+console.log(greaterOrEqual);
+console.log(lesserOrEqual);
+
+// Strict and loose equality - data type
+const c = 9;
+const d = "9";
+const cvalue = true;
+
+const loose = c == d; // true - checks only value, if they are equal then it's truthy
+const strict = c === d; // false - checks both value and datatype
+
+// Negation of strict and loose
+const negLoose = c != d;
+const negStrict = c !== d;
+
+// negation
+const neg = !cvalue;
+
+console.log(cvalue);
+console.log(loose);
+console.log(strict);
+console.log(negLoose);
+console.log(negStrict);
+console.log(neg);
---
+++ notes/js.md
@@ -0,0 +1,19 @@
+# Javascript Notes
+
+## Intro
+
+- HTML FILES ARE RENDERED FIRST, FOLLOWED BY CSS FILES AND THEN JS FILES GOES THROUGH THE BROWSER JAVASCRIPT ENGINES FOR EXAMPLE:
+  - CHROME: V8
+  - FIREFOX: SPIDERMONKEY
+  - SAFARI: NITRO
+
+## Xtics of programming languages
+
+1. It must have a storage item such as variables to store values for later/ future use
+2. It must have a loop system that allows repepitive jobs
+3. It must have an execution item e.g functions that allows us to execute commands and decisions
+4. It must have data-types or a data management system that allows us to distinguish between data of different types and use-case.
+
+## Objects
+
+Uses "key-value pair" or "property-value pair"
