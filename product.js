991ad37da300487bd648ba68e75b58b1f7aeafe7
Julius Gabriel Adebowale <juadebgabriel@gmail.com>
2026-08-15 13:23:48 +0100

    feat: fix

--- product.js
+++ product.js
@@ -40,6 +40,7 @@ const validateLogin = () => {
 
     isValid = false;
   }
+
   // Conditions 2 - if the password has empty value at submit
   if (!password) {
     passwordError.textContent = "Password is required";
@@ -58,11 +59,103 @@ const validateLogin = () => {
 };
 
 // Fetch the actual data
-const loginWithFetch = async (credentials) => {};
+const loginWithFetch = async (credentials) => {
+  const response = await fetch("https://dummyjson.com/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      ...credentials, // username and password
+      expiresInMins: 30,
+    }),
+    credentials: "include",
+  });
+  // If the data actually comes very well this is skipped
+  if (!response.ok) {
+    throw new Error("Invalid Login");
+  }
+  // Get info
+  const data = await response.json(); //Fullname, add, gender, dob, auth key/token
 
+  return data;
+};
 
-// FE(client) <-----> fetch / axios (messenger) <---> BE(server) <-> DB
+// // Axios
+// const loginWithAxios = async (credentials) => {
+//   const response = await axios.post(
+//     "https://dummyjson.com/auth/login",
+//     {
+//       ...credentials,
+//       expiresInMins: 30,
+//     },
+//     { withCredentials: true },
+//   );
 
+//   return response.data;
+// };
 
-Promise.reject
-Promise.resolve
+// To confirm the user and prints the info
+const getAuthUserWithFetch = async (accessToken) => {
+  const response = await fetch("https://dummyjson.com/auth/me", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Invalid Login");
+  }
+
+  return await response.json();
+};
+// // To confirm the user and prints the info : axios
+// const getAuthUserWithAxios = async (accessToken) => {
+//   const response = await axios("https://dummyjson.com/auth/me", {
+//     headers: {
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//     withCredentials: true,
+//   });
+
+//   return response.data;
+// };
+
+// Render user profile
+const renderProfile = (user) => {
+  profile.innerHTML = `
+  <img src="${user.image}" width="80" alt="${user.firstname}"/>
+  <h2>${user.firstname} ${user.lastName}</h2>
+  <p>${user.email}</p>
+  `;
+};
+
+// actual login trigger
+const handleLogin = async (event) => {
+  event.preventDefault();
+
+  if (!validateLogin()) {
+    return; // intentionally breaking the code
+  }
+
+  const credentials = {
+    username: usernameInput.value.trim(),
+    password: passwordInput.value.trim(),
+  };
+
+  loginStatus.textContent = "Logging in ....";
+
+  try {
+    const loginData = await loginWithFetch(credentials);
+    const user = await getAuthUserWithFetch(loginData.accessToken);
+
+    renderProfile(user);
+    saveSession(loginData.accessToken, user);
+
+    loginSection.hidden = true;
+    dashboard.hidden = false;
+
+    await loadProducts();
+  } catch (error) {
+    loginStatus.textContent = "Login Failed";
+  }
+};
